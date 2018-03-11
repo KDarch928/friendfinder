0550cb6e9524acff778584189dd5716d4756f7dd
KDarch928 <Kayle.Drucker@gmail.com>
2018-03-10 20:45:08 -0600

    worked on the logic of the bestmatch

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+var friendArray = [
+    {
+        name: "Jessica",
+        photo: "http://www.hawtcelebs.com/wp-content/uploads/2017/08/justina-machado-at-32nd-annual-imagen-awards-in-los-angeles-08-18-2017_1.jpg",
+        scores: [5,1,2,3,2,3,4,4,3,2]
+    },
+    {
+        name: "Frank",
+        photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2MjA4MDI2MV5BMl5BanBnXkFtZTcwNTk4MzU3NA@@._V1_SY1000_CR0,0,799,1000_AL_.jpg",
+        scores: [2,5,2,2,5,5,1,5,1,5]
+    },
+    {
+        name: "Ronny",
+        photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2NTg1OTQzMF5BMl5BanBnXkFtZTcwNjIyMjkyMg@@._V1_.jpg",
+        scores: [5,5,5,5,1,2,1,2,3,1]
+    },
+    {
+        name: "Sarah",
+        photo: "http://4.bp.blogspot.com/_OusZ7jLeKwg/TUzofDlM_FI/AAAAAAAAA1k/ca3XmsluLD0/s1600/Rachel+Greene+Most+Famous+Actress+%25287%2529.jpg",
+        scores: [1,3,3,1,2,2,5,2,3,2]
+    },
+    {
+        name: "Susan",
+        photo: "https://southcanara.in/wp-content/uploads/2015/09/freida-pinto1.jpg",
+        scores: [4,3,2,1,1,2,5,3,4,1]
+    },
+    {
+        name: "James",
+        photo: "http://mimg.ugo.com/201002/37594/cuts/actortayl-john-59525367-600_288x288.jpg",
+        scores: [4,2,3,4,3,4,4,3,2,3]
+    },
+    {
+        name: "Ken",
+        photo: "https://virily.com/wp-content/uploads/2018/01/surprisingly-short-actors.jpeg",
+        scores: [4,1,5,2,2,1,2,2,5,5]
+    },
+    {
+        name: "Nick",
+        photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMDAzNGI0NzEtZmJiMS00ODljLWI1MmUtNWNkZTZiNzA5NTQ0XkEyXkFqcGdeQXVyNTA0NjUwMDY@._V1_SY1000_CR0,0,800,1000_AL_.jpg",
+        scores: [2,4,3,4,1,4,2,3,1,5]
+    },
+    {
+        name: "Fiona",
+        photo: "https://ohmy.disney.com/wp-content/uploads/sites/25/2016/01/AmericaFerrera.jpg",
+        scores: [1,1,1,2,2,2,3,3,4,5]
+    }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,31 @@
 //load data
 var friendData = require("../data/friends");
 
+
+function checkForBestMatch(ursArrScore) {
+	var bestMatch;
+	var totalDiff = 0;
+	var newtotalDiff = 0;
+
+	for(var i = 0; i < friendData.length; i++){
+		for(j = 0; j < ursArrScore.length; j++){
+			newtotalDiff += Math.abs(friendData[i].scores[j] - ursArrScore[j]);
+		}
+		if(totalDiff === 0) {
+            totalDiff = newtotalDiff;
+            bestMatch = friendData[i];
+		} else if (totalDiff > newtotalDiff){
+			totalDiff = newtotalDiff;
+			bestMatch = friendData[i];
+		}
+		newtotalDiff = 0;
+
+	}
+
+	return bestMatch;
+
+}
+
 //routing
 
 module.exports = function(app) {
@@ -12,6 +37,28 @@ module.exports = function(app) {
 
 	//post (create new friends)
 	app.post("/api/friends",function(req, res){
+
+		var data = req.body;
+		var newIntArr = [];
+
+		for (var i = 0; i < data.scores.length; i++){
+			newIntArr.push(parseInt(data.scores[i]));
+		}
+
+		var newUsrData = {
+			name: data.name,
+			photo: data.photo,
+			scores: newIntArr
+		}
+
+		var bestMatch = checkForBestMatch(newIntArr);
+
+
+		friendData.push(newUsrData);
+        res.json(bestMatch);
+
+
+
 
 	});
 
